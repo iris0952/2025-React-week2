d3d35ee73d21c2bab1a2e0c62525397d6a1a7b01
Cami Wang <iris0952@gmail.com>
2026-02-05 23:30:59 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2025-React-week2/',
   build: {
     outDir: 'build' // 將輸出目錄設為 build
   }
